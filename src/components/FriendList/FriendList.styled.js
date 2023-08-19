e18c2be28120c4d37e96e27d3d39780844733680
Danylo Tytarenko <121917694+DanyloTytarenko@users.noreply.github.com>
2023-08-20 01:18:56 +0300

    finish

---
+++ src/components/FriendList/FriendList.styled.js
@@ -0,0 +1,9 @@
+import { styled } from 'styled-components';
+
+export const List = styled.ul`
+  margin: 80px auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 10px;
+`;
